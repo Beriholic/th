503509e1656a56e09858aa35c5c76b74c1e09a9f
Beriholic <96678707+Beriholic@users.noreply.github.com>
2023-11-18 21:51:34 +0800

    [feat]Add parametric and parametric modes

--- cmd/restore.go
+++ cmd/restore.go
@@ -16,45 +16,90 @@ var fileInfos []handler.Info
 var isEmpty bool = false
 
 var restoreCmd = &cobra.Command{
-	Use:   "restore",
-	Short: "restore a file from trash",
+	Use:     "restore",
+	Aliases: []string{"r", "rs"},
+	Short:   "restore a file from trash",
+	Args:    cobra.MinimumNArgs(0),
 	PreRun: func(cmd *cobra.Command, args []string) {
-		var err error
-		fileInfos, err = handler.ShowTable()
-		if err != nil {
-			fmt.Println(err)
-		}
-
-		if len(fileInfos) == 0 {
-			isEmpty = true
+		if len(args) == 0 {
+			PostHandleWithNoArgs()
+		} else {
+			PostHandleWithArgs()
 		}
 	},
 	Run: func(cmd *cobra.Command, args []string) {
 		if isEmpty {
 			return
 		}
-		fmt.Println("input the id of the file you want to restore")
-		nums := make([]int, 0)
-		input := Read()
-		input = strings.Replace(input, "\n", "", -1)
-		// fmt.Println("input --> ", input)
-		for _, v := range strings.Split(input, " ") {
-			num, err := strconv.Atoi(v)
-			if err != nil {
-				fmt.Println(err)
-				return
+		if len(args) == 0 {
+			HandleWithNoArgs()
+		} else {
+			fmt.Println("ok")
+			nums := make([]int, 0)
+			for _, v := range args {
+				num, err := strconv.Atoi(v)
+				if err != nil {
+					fmt.Println(err)
+					return
+				}
+				nums = append(nums, num)
 			}
-			nums = append(nums, num)
-		}
-
-		if err := handler.Restore(nums, fileInfos); err != nil {
-			fmt.Println(err)
-			return
+			HandleWithArgs(nums)
 		}
 	},
 	PostRun: func(cmd *cobra.Command, args []string) {
 		fmt.Println("ALL Done!")
 	},
+}
+
+func PostHandleWithArgs() {
+	var err error
+	fileInfos, err = handler.GetTrashList()
+	if err != nil {
+		fmt.Println(err)
+	}
+	if len(fileInfos) == 0 {
+		isEmpty = true
+	}
+}
+func PostHandleWithNoArgs() {
+	var err error
+	fileInfos, err = handler.ShowTable()
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	if len(fileInfos) == 0 {
+		isEmpty = true
+	}
+
+}
+func HandleWithArgs(num []int) {
+	if err := handler.Restore(num, fileInfos); err != nil {
+		fmt.Println(err)
+		return
+	}
+}
+func HandleWithNoArgs() {
+	fmt.Println("input the id of the file you want to restore")
+	nums := make([]int, 0)
+	input := Read()
+	input = strings.Replace(input, "\n", "", -1)
+	// fmt.Println("input --> ", input)
+	for _, v := range strings.Split(input, " ") {
+		num, err := strconv.Atoi(v)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		nums = append(nums, num)
+	}
+
+	if err := handler.Restore(nums, fileInfos); err != nil {
+		fmt.Println(err)
+		return
+	}
+
 }
 
 func Read() string {
