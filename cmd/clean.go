d03b075dfbed2382317756507790164d21067746
Beriholic <96678707+Beriholic@users.noreply.github.com>
2023-11-19 11:35:30 +0800

    [feat]Add options

--- cmd/clean.go
+++ cmd/clean.go
@@ -3,6 +3,7 @@ package cmd
 import (
 	"fmt"
 
+	"github.com/Beriholic/th/consts/errs"
 	"github.com/Beriholic/th/handler"
 	"github.com/spf13/cobra"
 )
@@ -11,10 +12,22 @@ var cleanCmd = &cobra.Command{
 	Use:   "clean",
 	Short: "Clean all files in Trash",
 	Run: func(cmd *cobra.Command, args []string) {
-		err := handler.Clean()
-		if err != nil {
-			fmt.Println(err)
+		fmt.Println("The trash will be cleaned, do you want to continue? [y/n]")
+		var input string
+		fmt.Scanln(&input)
+
+		switch input {
+		case "y", "Y":
+			err := handler.Clean()
+			if err != nil {
+				fmt.Println(err)
+			}
+		case "n", "N":
+			return
+		default:
+			fmt.Println(errs.ErrInvalidInput)
 		}
+
 	},
 }
 
